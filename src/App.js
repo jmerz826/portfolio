d87f6fd26cfb0c089024ed45af534b3d5305b1af
John Merz <jmerz826@gmail.com>
2022-01-22 16:03:34 -0500

    router and switch set up

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
+import Header from "./components/header";
+import { Route, Switch } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
+      <Header />
+      <Switch>
+        <Route path='/'>
+          <h2>path</h2>
+        </Route>
+      </Switch>
     </div>
   );
 }
---
+++ src/components/header.js
@@ -0,0 +1,12 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>John Merz</h1>
+            <nav>
+                
+            </nav>
+        </header>
+    )
+}
+
+export default Header;
