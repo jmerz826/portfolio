6db0a0e510ce7afbaf288c82a156bc90090d57f6
John Merz <jmerz826@gmail.com>
2022-02-15 23:31:10 -0500

    family recipes project added

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,11 +1,28 @@
 const projects = [
     {
         name: 'Shop Smart',
-        description: 'Shop Smart is my first notable independent project. The project serves as a shopping companion app, implementing unique methods of creating and managing shopping lists. The project showcases material learned in the first half of BloomTech\'s full stack program, including ReactJS, React-Redux, JavaScript, CSS, and HTML. All work done is entirely my own.',
+        description: 'Shop Smart is my first notable independent project. The project serves as a shopping companion app, implementing unique methods of creating and managing shopping lists. The project showcases material learned in the first half of BloomTech\'s full stack program.',
+        tools: ['ReactJS', 'React-Redux', 'JavaScript', 'CSS', 'HTML', 'Heroku' ],
         live_link: 'https://shop-smart.vercel.app/',
         github_link: 'https://github.com/jmerz826/shop-smart',
         time: '12/21-1/22'
-    }
+    },
+    {
+        name: 'Secret Family Recipes',
+        description: 'Secret Family Recipes is a group project completed for my final build week at BloomTech. I, personally, spearheaded the backend development',
+        tools: ['Node.js', 'Express', 'PostgreSQL', 'knex.js', 'jsonwebtoken', 'bcrypt', 'Heroku'],
+        live_link: 'http://secret-family-recipes-cookbook-8.vercel.app/',
+        github_link: 'https://github.com/secret-family-recipes-08',
+        time: '1/22-2/22'
+    },
+    {
+        name: 'John\'s JavaScript Arcade',
+        description: 'Secret Family Recipes is a group project completed for my final build week at BloomTech. I, personally, spearheaded the backend development',
+        tools: ['Node.js', 'Express', 'PostgreSQL', 'knex.js', 'jsonwebtoken', 'bcrypt', 'Heroku'],
+        live_link: 'https://shop-smart.vercel.app/',
+        github_link: 'https://github.com/jmerz826/shop-smart',
+        time: '1/22-2/22'
+    },
 ];
 
 export default projects;
