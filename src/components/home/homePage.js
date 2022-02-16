c349f3b4ce1a03be4a1b796712833216ac7d2c98
John Merz <jmerz826@gmail.com>
2022-02-15 23:31:22 -0500

    home page formatting

--- src/components/home/homePage.js
+++ src/components/home/homePage.js
@@ -1,19 +1,24 @@
 import React from "react";
 import styled from "styled-components";
-import headshot from '../../images/linkedinPhoto.jpg'
+import headshot from "../../images/linkedinPhoto.jpg";
 
 const StyledHomePage = styled.div`
-    #about-me{
-        background-color: navy;
-    }
-    #content-container{
-        display:flex;
-    }
-    img{
-      width: 20%;
-      border-radius: 9999px;
-    }
-`
+  #content-container {
+    display: flex;
+  }
+  #about-me {
+    background-color: navy;
+    width: 50%;
+  }
+  #projects-container {
+    background-color: gold;
+    width: 50%;
+  }
+  img {
+    width: 50%;
+    border-radius: 9999px;
+  }
+`;
 
 const HomePage = () => {
   return (
@@ -22,12 +27,24 @@ const HomePage = () => {
       <div id="content-container">
         <div id="about-me">
           <h3>About Me</h3>
-          <img src={headshot} alt='headshot'/>
-                  <p>My name is John, and I am an engineer at heart. I posess a bachelor's degree in mechanical engineering, and am currently studying full stack web development at <a href="https://www.bloomtech.com/courses/full-stack-web-development" target={'_blank'} rel="noreferrer">Bloom Institute of Technology</a>. I am passionate about development because the field is constantly evolving, and there never a shortage of new things to learn.</p>
+          <img src={headshot} alt="headshot" />
+          <p>
+            My name is John, and I am an engineer at heart.
+            <br /> I posess a bachelor's degree in mechanical engineering, and
+            am currently studying full stack web development at{" "}
+            <a
+              href="https://www.bloomtech.com/courses/full-stack-web-development"
+              target={"_blank"}
+              rel="noreferrer"
+            >
+              Bloom Institute of Technology
+            </a>
+            . I am passionate about development because the field is constantly
+            evolving, and there never a shortage of new things to learn.
+          </p>
         </div>
-        <div>
-                  <h3>Projects</h3>
-                  
+        <div id="projects-container">
+          <h3>Projects</h3>
         </div>
       </div>
     </StyledHomePage>
