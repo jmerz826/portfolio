04ffde29c2b0b6fe81732ec25037fb7784d672ba
John Merz <jmerz826@gmail.com>
2022-02-17 11:46:58 -0500

    header and ticker fonts

--- src/components/header.js
+++ src/components/header.js
@@ -2,23 +2,24 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const StyledHeader = styled.header`
-    display: flex;
-    justify-content: space-between;
-    background-color: darkcyan;
-    padding: 1% 2%;
-`
+  font-family: "Yellowtail", cursive;
+  display: flex;
+  justify-content: space-between;
+  background-color: darkcyan;
+  padding: 1% 2%;
+`;
 
 const Header = () => {
-    return (
-        <StyledHeader>
-            <h2>John Merz</h2>
-            <nav>
-                <Link to='/'>Home</Link>
-                <Link to='/projects'>Projects</Link>
-                <Link to='/contact'>Contact</Link>
-            </nav>
-        </StyledHeader>
-    )
-}
+  return (
+    <StyledHeader>
+      <h2>John Merz</h2>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/projects">Projects</Link>
+        <Link to="/contact">Contact</Link>
+      </nav>
+    </StyledHeader>
+  );
+};
 
 export default Header;
