6dc99acc4ee1bacbda758624a42f631125c83eba
John Merz <jmerz826@gmail.com>
2022-02-18 15:42:52 -0500

    made projects page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Footer from "./components/footer";
 import { Route, Switch } from 'react-router-dom';
 import HomePage from "./components/home/homePage";
 import ContactPage from "./components/contactPage";
+import Projects from "./components/projectsPage/projects";
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           <HomePage />
         </Route>
         <Route path='/projects'>
-
+          <Projects />
         </Route>
         <Route path='/contact'>
           <ContactPage />
---
+++ src/components/projectsPage/projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledDiv = styled.div``
+
+const Projects = () => {
+    return (
+        <StyledDiv>
+            <h2>hi</h2>
+        </StyledDiv>
+    );
+};
+
+export default Projects;
