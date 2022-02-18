7ceddd3f813044c78c5a72adc2ccf6efe8ab8434
John Merz <jmerz826@gmail.com>
2022-02-18 00:06:45 -0500

    added to footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom';
 
 const StyledFooter = styled.footer`
     text-align:center;
     padding: 1%;
+    border-top: 2px solid black;
+    a{
+        font-size: inherit;
+        color: inherit;
+        text-decoration: none;
+        font-style: italic;
+    }
 `
 
 const Footer = () => {
     return (
         <StyledFooter>
             <h5>⚡💻⚡ Site created exclusively by John Merz ⚡💻⚡</h5>
+            <h5>Let's talk! <Link to="/contact">🗣📧Contact Me!📧🗣</Link></h5>
         </StyledFooter>
     );
 };
