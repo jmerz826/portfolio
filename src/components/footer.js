983306d63a15e6da6c1b844240f294d0c8865503
John Merz <jmerz826@gmail.com>
2022-02-20 16:52:59 -0500

    fixed footer link

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,27 +1,35 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Link } from 'react-router-dom';
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const StyledFooter = styled.footer`
-    text-align:center;
-    padding: 1%;
-    border-top: 2px solid black;
-    a{
-        font-size: inherit;
-        color: inherit;
-        text-decoration: none;
-        font-style: italic;
-    }
-`
+  text-align: center;
+  padding: 1%;
+  border-top: 2px solid black;
+  a {
+    font-size: inherit;
+    color: inherit;
+    text-decoration: none;
+    font-style: italic;
+  }
+`;
 
 const Footer = () => {
-    return (
-        <StyledFooter>
-            <h5>Let's talk! <Link to="/contact">🗣📧Contact Me!📧🗣</Link></h5>
-            <h5>⚡💻⚡ Site created exclusively by John Merz ⚡💻⚡</h5>
-            <Link to="https://github.com/jmerz826/portfolio" target="_blank">This Site's GitHub! 🔗</Link>
-        </StyledFooter>
-    );
+  return (
+    <StyledFooter>
+      <h5>
+        Let's talk! <Link to="/contact">🗣📧Contact Me!📧🗣</Link>
+      </h5>
+      <h5>⚡💻⚡ Site created exclusively by John Merz ⚡💻⚡</h5>
+      <a
+        href="https://github.com/jmerz826/portfolio"
+        target="_blank"
+        rel="noreferrer"
+      >
+        This Site's GitHub! 🔗
+      </a>
+    </StyledFooter>
+  );
 };
 
 export default Footer;
